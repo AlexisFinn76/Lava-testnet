4eedb3f08abb0b9e2ca7d1512bbe488a6954e30f
AlexisFinn76 <alexisfinn763@gmail.com>
2024-06-11 14:00:33 +0100

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-e02a564b-5981-4c1f-b488-40568585fcc0/"
 
 OUTPUT_DIR="logs"
 
